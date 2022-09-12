7dcb5300983f2849f2df6b33b2062cb4b6a22cf0
Sebastian Porras <sporras@vertafore.com>
2022-09-12 00:35:49 -0600

    todo está listo

--- programaestudiante.py
+++ programaestudiante.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import (IntegerType,StringType, StructField,StructType,FloatType,DateType)
 import sys
-from functions import read_files,total_viajes,total_ingresos
+from functions import read_files,total_viajes,total_ingresos,metricas
 #inputs
 
 
@@ -9,11 +9,13 @@ from functions import read_files,total_viajes,total_ingresos
 #spark session
 
 df=read_files()
-df.printSchema()
+#df.printSchema()
 #df.show(50)
 
 viajes=total_viajes(df)
-viajes.show(50)
+#viajes.show(50)
 
 ingresos= total_ingresos(df)
-ingresos.show(50)
+#ingresos.show(50)
+
+metricass = metricas(df)
